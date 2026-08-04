d1e26280aad6f190504a60b689c0c5d9f0cabe36
natalia agapova <agapnatalya004@gmail.com>
2026-08-04 22:06:46 +0300

    updated store_embeddings in chroma_db.py

--- src/chroma_db.py
+++ src/chroma_db.py
@@ -5,17 +5,34 @@ def create_database():
     collection = client.get_or_create_collection("documents")
     return collection
 
-def store_embeddings(collection, chunks, embeddings):
+def store_embeddings(collection, chunks, embeddings, filename):
     """
     each record needs three things:
     1. an ID number
     2. the text chunk
     3. the embedding vector
+    4. the filename
     """
+
+    documents = [] # strings only
+    metadata = [] # filename + pages per chunk
+
+    for chunk in chunks:
+        documents.append(chunk["text"])
+
+        page_numbers = chunk["page_numbers"]
+        pages_string = ",".join(str(p) for p in page_numbers)
+
+        metadata.append({
+            "filename": filename,
+            "page_numbers": pages_string,
+        })
+
     collection.add(
         ids=[str(i) for i in range(len(chunks))],
-        documents = chunks,
+        documents = documents, # strings/chunks
         embeddings = embeddings,
+        metadatas = metadata, # citation information for each chunk
     )
 
 def search_database(collection, query_embedding):
